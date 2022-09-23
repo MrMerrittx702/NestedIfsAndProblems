4b0e1a43c20f6c75b81a3f908c56f7c532d6efbb
Evan Merritt <109871289+MrMerrittx702@users.noreply.github.com>
2022-09-23 00:24:48 -0500

    Create NestedIfsAndProblems.java

---
+++ NestedIfsAndProblems.java
@@ -0,0 +1,30 @@
+/*
+Develop a program that performs the following:
+  Assuming that the variable n has been properly declared and initialized
+  Create a method that accepts an integer n and 
+
+  If n is even check if n is divisible by 5 
+  If n is divisible by 5 check if n is divisible by 10.
+
+  If n is odd check if n is divisible by 3.
+  If n is divisible by 3 check if n is divisible by 7.
+
+  return the largest number n is divisible by
+  If only odd return 1
+  If only even return 2
+
+  Print your return value. 
+
+*/
+import java.util.Scanner;
+
+public class NestedIfsAndProblems{
+  public static void main(String[] args){
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt()
+//Start your code after this line---------------------------------------------------------------------------------------------
+// use n in place of an integer literal in you method calls-------------------------------------------------------------------
+  
+  }
+
+}
